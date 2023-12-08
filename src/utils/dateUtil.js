02d010bb63dcbaa986cdc20bc5af0b5e735e0c52
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-08 16:55:31 +0900

    Feat: 날짜 관련 상수 및 util 함수 기능

---
+++ src/utils/dateUtil.js
@@ -0,0 +1,30 @@
+import {
+  addDays,
+  differenceInCalendarDays,
+  endOfMonth,
+  endOfWeek,
+  startOfMonth,
+  startOfWeek,
+} from "date-fns";
+
+export const getMonthDetails = (monthId) => {
+  const month = monthId || 1; // {param} monthId가 없으면 1월로 설정
+  const currentDate = new Date(2024, month - 1); // {param} month의 첫 날짜
+  const monthStart = startOfMonth(currentDate); // {param} month의 첫 날짜
+  const monthEnd = endOfMonth(currentDate); // {param} month의 마지막 날짜
+  const startDate = startOfWeek(monthStart); // {param} month의 첫 주의 첫 날짜
+  const endDate = endOfWeek(monthEnd); // {param} month의 마지막 주의 마지막 날짜
+
+  return { month, currentDate, monthStart, monthEnd, startDate, endDate };
+};
+
+export const getMonthDate = (monthDetails) => {
+  const dateArray = [];
+  let day = monthDetails.startDate;
+  while (differenceInCalendarDays(monthDetails.endDate, day) >= 0) {
+    dateArray.push(day);
+    day = addDays(day, 1);
+  }
+
+  return dateArray;
+};
