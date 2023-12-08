ea7f9b3315cb9c33712a4d5e4010556f2cb63319
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-08 16:57:50 +0900

    Feat: 월별 달력 페이지 생성

--- src/pages/MonthPage.jsx
+++ src/pages/MonthPage.jsx
@@ -1,3 +1,37 @@
+import Layout from "@components/common/Layout";
+import Header from "@components/common/Header";
+import TextBox from "@components/common/TextBox";
+import WeekDays from "@components/calendar/Weekdays";
+import DateCell from "@components/calendar/DateCell";
+import { useParams } from "react-router-dom";
+import { getMonthDate, getMonthDetails } from "@utils/dateUtil";
+
 export default function MonthPage() {
-  return <>MonthPage</>;
+  const param = useParams();
+  const monthDetails = getMonthDetails(param.monthId);
+  const monthDates = getMonthDate(monthDetails);
+
+  return (
+    <Layout>
+      <Header />
+
+      <TextBox className="px-4 font-semibold">
+        2024년 {monthDetails.month}월
+      </TextBox>
+
+      {/* 요일 표시 */}
+      <WeekDays />
+
+      {/* 날짜 표시 */}
+      <div className="grid grid-cols-7 border-t-[1px] border-l-[1px] border-[#f3f4f5]">
+        {monthDates.map((date, index) => (
+          <DateCell
+            key={`date${index}`}
+            date={date}
+            currentDate={monthDetails.currentDate}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
 }
