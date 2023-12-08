6149dab1b2de390c18dae68905841d97c86ae2d2
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-08 16:57:32 +0900

    Feat: 메인 페이지 생성

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,3 +1,27 @@
+import Layout from "@components/common/Layout";
+import Header from "@components/common/Header";
+import TextBox from "@components/common/TextBox";
+import { MONTHS } from "@constants/dateConstants";
+import { Link } from "react-router-dom";
+
 export default function MainPage() {
-  return <>MainPage</>;
+  return (
+    <Layout>
+      <Header />
+      <TextBox className="text-center py-5">
+        월별로 인재님의 소망과 사진을 기록해보세요
+      </TextBox>
+      <section className="grid grid-cols-3 mx-1 gap-1">
+        {MONTHS.map((month) => (
+          <Link
+            key={month}
+            to={`/month/${month}`}
+            className="py-10 border rounded-xl text-center"
+          >
+            {month}월
+          </Link>
+        ))}
+      </section>
+    </Layout>
+  );
 }
