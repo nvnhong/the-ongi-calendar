a76dacf991a59d3d0f8fca76d80a6c0cbf6c4f40
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-09 22:31:27 +0900

    Feat: mui의 date picker 설정(기본설정 및 한글 세팅)

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import ko from "date-fns/locale/ko";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ko}>
+      <RouterProvider router={router} />
+    </LocalizationProvider>
   </QueryClientProvider>
 );
