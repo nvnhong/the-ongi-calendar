83f9af7b679bb5dc8c39a69fcabcfd7e70858aa5
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-09 22:31:40 +0900

    Feat: 서버측으로 request 시 헤더에 토큰 추가 기능

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,19 @@
 import axios from "axios";
+import { getCookie } from "@utils/cookieUtil";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  // 서버 요청 시 쿠키에 토큰이 존재하면 header에 토큰을 추가한다.
+  const accessToken = getCookie("accessToken");
+  if (accessToken) {
+    config.headers["Authorization"] = accessToken;
+    config.withCredentials = true;
+  }
+  return config;
 });
