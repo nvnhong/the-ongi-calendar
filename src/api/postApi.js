fc7863e8646dfc353800784d1b651d2fcb78a95f
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-10 15:23:20 +0900

    Feat: api 추가(게시글 수정, 게시글 삭제)

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -4,3 +4,13 @@ export const postUpload = async (data) => {
   const response = await axiosInstance.post("/api/year-end", data);
   return response;
 };
+
+export const postUpdate = async (postId, post) => {
+  const response = await axiosInstance.put(`/api/year-end/${postId}`, post);
+  return response;
+};
+
+export const postDelete = async (postId) => {
+  const response = await axiosInstance.delete(`/api/year-end/${postId}`);
+  return response;
+};
