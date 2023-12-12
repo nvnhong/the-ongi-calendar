3b2b915c9dd227ff0f1fb13693fce65c7fe60969
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-12 12:39:11 +0900

    Feat: 일별 소망 보기 기능 추가 위해 onClick 추가

--- src/components/calendar/DateCell.jsx
+++ src/components/calendar/DateCell.jsx
@@ -1,16 +1,17 @@
 import { format, getMonth } from "date-fns";
 
-export default function DateCell({ date, currentDate, isEventDay }) {
+export default function DateCell({ date, currentDate, isEventDay, onClick }) {
   const validation = getMonth(currentDate) === getMonth(date);
 
   return (
     <div
       className={`px-1 py-2 border-r-[1px] border-b-[1px] border-[#f3f4f5] cursor-pointer text-center ${
-        validation ? "text-[#4a5261]" : "text-[#8c919c] bg-[#f9fafc]"
+        validation ? "text-[#4a5261]" : ""
       } ${isEventDay && "bg-blue-50"}`}
+      onClick={onClick}
     >
       <div>
-        <span>{format(date, "d")}</span>
+        <span>{validation ? format(date, "d") : ""}</span>
       </div>
     </div>
   );
