4b922c913ed0e4d2530de4da143ce54d2e1d7fdd
hongmikyeong <mikyeong.dev@gmail.com>
2023-12-14 18:43:51 +0900

    Design: Header 상단 고정

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,6 +1,19 @@
+import React from "react";
+import Header from "./Header";
+
 export default function Layout({ children }) {
+  const childArray = React.Children.toArray(children);
+
+  const isHeaderPresent = childArray.some(
+    (child) => React.isValidElement(child) && child.type === Header
+  );
+
   return (
-    <div className="w-[360px] h-screen mx-auto bg-white">
+    <div
+      className={`w-[360px] h-screen mx-auto bg-white ${
+        isHeaderPresent && "pt-[60px]"
+      }`}
+    >
       <div>{children}</div>
     </div>
   );
